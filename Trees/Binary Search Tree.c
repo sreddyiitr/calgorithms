1a68746b8ff6ee184093770e3a7ade001f1b14a8
sreddyiitr <sunilleo@gmail.com>
2022-08-20 23:21:25 -0500

    Update Binary Search Tree.c

--- Trees/Binary Search Tree.c
+++ Trees/Binary Search Tree.c
@@ -1,5 +1,5 @@
 /******************************************************************************
- * AVL Tree - Search, Insert and Delete
+ * Binary Search Tree - Search, Insert and Delete
  * 
 *******************************************************************************/
 #include <stdio.h>
@@ -8,20 +8,7 @@
 struct Node {
     int key;
     struct Node *left, *right;
-    int height;
 };
-
-int max(int a, int b) {
-
-    return a>b?a:b;    
-}
-
-int get_height(struct Node *node) {
-    if(node==NULL) {
-        return 0;
-    }
-    return node->height;
-}
 
 struct Node * new_node(int key) {
     // Let's create a node that can be used in tree later
@@ -29,54 +16,7 @@ struct Node * new_node(int key) {
     temp->left = NULL;
     temp->key = key;
     temp->right = NULL;
-    temp->height = 1;
     return temp; 
-}
-
-int get_balance_factor(struct Node *node) {
-    if(node == NULL) {
-        return 0;
-    }
-    return get_height(node->left) - get_height(node->right);
-}
-
-/****
- *      --------y-------
- *  ----x----               T3
- *  T1      T2
- * */
-struct Node * right_rotate(struct Node* y) {
-    
-    struct Node* x = y->left;
-    struct Node* T2 = x->right;
-    
-    x->right = y;
-    y->left = T2;
-    
-    y->height = 1 + max(get_height(y->right), get_height(y->left));
-    x->height = 1 + max(get_height(x->right), get_height(x->left));
-    
-    return x; // x is the new root
-}
-
-/**
-*                       x
-*           T1                          y
-*                               T2              T3
-*
-*/
-struct Node * left_rotate(struct Node* x) {
-    
-    struct Node * y = x->right;
-    struct Node * T2 = y->left;
-    
-    y->left = x;
-    x->right = T2;
-    
-    y->height = 1 + max(get_height(y->right), get_height(y->left));
-    x->height = 1 + max(get_height(x->right), get_height(x->left));
-    
-    return y; // y is the new root
 }
 
 /*
@@ -95,64 +35,26 @@ struct Node * insert(struct Node * node, int key) {
     } else if(key > node->key) {
         node->right = insert(node->right, key);
     }
-    
-    node->height = 1 + max(get_height(node->left) , get_height(node->right));
-    int balanceFactor = get_balance_factor(node);
-    
-    // Left Left 
-    if(balanceFactor > 1 && key < node->left->key) {
-        return right_rotate(node);
-    }
-    // Right Right
-    if(balanceFactor < -1 && key > node->right->key) {
-        return left_rotate(node);
-    }
-    // Left Right
-    if(balanceFactor > 1 && key > node->left->key) {
-        node->left = left_rotate(node->left);
-        return right_rotate(node);
-    }
-    // Right Left
-    if(balanceFactor < -1 && key < node->right->key) {
-        node->right = right_rotate(node->right);
-        return left_rotate(node);
-    }
     return node; 
 }
 
-void preOrder(struct Node * root) {
+void inorder(struct Node *root) {
     
     if(root != NULL) {
+        inorder(root->left);
         printf("%d ", root->key);
-        preOrder(root->left);
-        preOrder(root->right);
-    }    
-}
-
-void inOrder(struct Node * root) {
-    
-    if(root != NULL) {
-        inOrder(root->left);
-        printf("%d ", root->key);
-        inOrder(root->right);
-    }    
-}
-
-void postOrder(struct Node * root) {
-
-    if(root != NULL) {
-        postOrder(root->left);
-        postOrder(root->right);
-        printf("%d ", root->key);
-    }    
-}
-
-struct Node * inorderPrecessor(struct Node * root) {
-    root = root->left;
-    while(root->right != NULL) {
-        root = root->right;
+        inorder(root->right);
     }
-    return root;
+}
+
+void postorder(struct Node *root) {
+
+        
+    if(root != NULL) {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->key);
+    }
 }
 
 struct Node * delete_node(struct Node * root, int key) {
@@ -192,48 +94,25 @@ struct Node * delete_node(struct Node * root, int key) {
           
     }
     
-    root->height = 1 + max(get_height(root->left) , get_height(root->right));
-    int balanceFactor = get_balance_factor(root);
-    
-    // Left Left 
-    if(balanceFactor > 1 && key < root->left->key) {
-        return right_rotate(root);
-    }
-    // Right Right
-    if(balanceFactor < -1 && key > root->right->key) {
-        return left_rotate(root);
-    }
-    // Left Right
-    if(balanceFactor > 1 && key > root->left->key) {
-        root->left = left_rotate(root->left);
-        return right_rotate(root);
-    }
-    // Right Left
-    if(balanceFactor < -1 && key < root->right->key) {
-        root->right = right_rotate(root->right);
-        return left_rotate(root);
-    }
     return root;
 
 }
 
-int main() {
+void main() {
+    struct Node *root = new_node(50);
+    insert(root, 30);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 70);
+    insert(root, 60);
+    insert(root, 80);
     
-    struct Node * root = NULL;
-    root = insert(root, 1);
-    root = insert(root, 2);
-    root = insert(root, 4);
-    root = insert(root, 5);
-    root = insert(root, 6);
-    root = insert(root, 3);
-    printf("\npostOrder | ");
-    postOrder(root);
-    printf("\npreOrder  | ");
-    preOrder(root);
-    int delete_node_key = 4;
-    printf("\nDeleting node with key: %d ", delete_node_key);
-    delete_node(root, delete_node_key);
-    printf("\npostOrder | ");
-    postOrder(root);
-    return 0;
+    inorder(root);
+    printf("\n");
+    postorder(root);
+    
+    delete_node(root, 50);
+    printf("\n");
+    inorder(root);
+
 }
